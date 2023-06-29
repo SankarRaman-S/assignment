a3492f02c83c69bb8e6f8c0bff1a0e79653c4736
SankarRaman <sankarraman.ssr@gmail.com>
2023-06-29 14:05:13 +0200

    Javadoc for the methods.

--- src/main/java/com/serand/assignment/model/Question.java
+++ src/main/java/com/serand/assignment/model/Question.java
@@ -14,9 +14,24 @@ import java.util.Map;
 public class Question {
     @Id
     private String id;
+    /**
+     * Question details to be displayed
+     */
     private String question;
+    /**
+     * Set of answers for the question
+     */
     private List<String> answers;
+    /**
+     * Set of correct answers if case of multiples
+     */
     private List<String> correctAnswers;
+    /**
+     * Set of user answers
+     */
     private List<String> userAnswers;
+    /**
+     * Answer points to calculate score
+     */
     private Map<String,Integer> points;
 }
--- src/main/java/com/serand/assignment/util/Util.java
+++ src/main/java/com/serand/assignment/util/Util.java
@@ -10,6 +10,28 @@ public class Util {
 
     public static final String COMPLETED = "completed";
 
+    /**
+     * Method calculates the score for the survey.
+     * Stream of survey question is passed to method calculate(userAnswers, correctAnswers, points)
+     * calculate method does the below operation
+     * 1. Filter the correct answers.
+     * 2. Get the points for each of the correct answer.
+     * 3. Sum the points for the correct answer.
+     * @param questions List of Questions for the survey.
+     * @return the score for correct answers.
+     */
+    public static long calculateScore(List<Question> questions) {
+        return questions.stream()
+                .map(question -> calculate(question.getUserAnswers(), question.getCorrectAnswers(), question.getPoints()))
+                .collect(Collectors.summingLong(Long::longValue));
+    }
+
+    /**
+     * @param userAnswers Answers by the users.
+     * @param correctAns Correct answers defined by the system.
+     * @param points Points for the correct answers.
+     * @return the calculated score for the correct answers based on the points
+     */
     public static Long calculate (List<String> userAnswers, List<String> correctAns, Map<String,Integer> points){
         return userAnswers.stream()
                 .filter(correctAns::contains)
@@ -17,6 +39,11 @@ public class Util {
                 .mapToLong(Long::longValue).sum();
     }
 
+    /**
+     * @param answer correct answer
+     * @param points points map for the correct answers Answer as key and point as value
+     * @return the point of the correct answer.
+     */
     private static long getPoints(String answer, Map<String, Integer> points) {
         if(points.containsKey(answer)){
             return points.get(answer);
@@ -24,12 +51,6 @@ public class Util {
         return 0;
     }
 
-    public static long calculateScore(List<Question> questions) {
-        return questions.stream()
-                .map(question -> calculate(question.getUserAnswers(), question.getCorrectAnswers(), question.getPoints()))
-                .collect(Collectors.summingLong(Long::longValue));
 
-
-    }
 
 }
